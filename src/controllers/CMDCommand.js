283e3d78cc763b5dc986e8556d3a4c14bfee9195
rodrigofelipe3 <rodrigo.soeu@hotmail.com>
2024-10-23 17:02:24 -0300

    feat cmdCommand.js

--- src/controllers/CMDCommand.js
+++ src/controllers/CMDCommand.js
@@ -4,109 +4,141 @@ const sudo = require('sudo-prompt')
 
 const ws = new WebSocket.Server({ port: 5002 })
 
-const CloseAllConnections = () => { 
-  ws.clients.foreach(client => { 
-    if(client.readState === WebSocket.OPEN) { 
+const closeAllConnections = () => {
+  ws.clients.forEach(client => {
+    if (client.readState === WebSocket.OPEN) {
       client.close()
     }
   })
 }
 
+let execute = false
 
 
 const ChkDsk = async () => {
   ws.on('connection', (ws) => {
-    console.log('Conectado ao servidor WebSocket do administrador.');
-    sudo.exec('sfc /scannow', { name: 'sfc' }, (error, stdout, stderr) => {
-      if (stderr) {
-        ws.send(stderr.toString('utf-8'))
-        ws.close()
-      }
-      if (stdout) {
+    console.log('Conectado ao servidor CheckDisk.');
+    if (!execute) {
+      execute = true
+      sudo.exec('chkdsk', { name: 'chkdsk' }, (error, stdout, stderr) => {
+        if (stderr) {
+          ws.send(stderr.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (stdout) {
+          console.log(stdout)
+          ws.send(stdout)
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (error) {
+          ws.send(error.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
 
-        ws.send(stdout.toString('utf-8'))
-        ws.close()
-      }
-      if(error){ 
-        ws.send(error.toString('utf-8'))
-        ws.close()
-      }
-    })
+      })
+    }
   })
-  ws.on('close', ()=> { 
-    ws.close()
-  })
-
 }
 
 const SystemFileCheck = () => {
   ws.on('connection', (ws) => {
-    console.log('Conectado ao servidor WebSocket do administrador.');
-    sudo.exec('sfc /scannow', { name: 'sfc' }, (error, stdout, stderr) => {
-      if (stderr) {
-        ws.send(stderr.toString('utf-8'))
-        ws.close()
-      }
-      if (stdout) {
+    console.log('Conectado ao servidor SFC SCANNOW.');
+    if (!execute) {
+      execute = true
+      sudo.exec('sfc /scannow', { name: 'sfc' }, (error, stdout, stderr) => {
+        if (stderr) {
+          ws.send(stderr.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (stdout) {
+          console.log(stdout)
+          ws.send(stdout.toString())
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+         
+        }
+        if (error) {
+          ws.send(error.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
 
-        ws.send(stdout.toString('utf-8'))
-        ws.close()
-      }
-      if(error){ 
-        ws.send(error.toString('utf-8'))
-        ws.close()
-      }
-    })
+      })
+    }
   })
-  ws.on('close', ()=> { 
-    ws.close()
-  })
+  ws.close()
+
 }
 
 const ScanHealth = () => {
   ws.on('connection', (ws) => {
-    console.log('Conectado ao servidor WebSocket do administrador.');
-    sudo.exec('dism /online /cleanup-image /scanhealth', { name: 'checkhealth' }, (error, stdout, stderr) => {
-      if (stderr) {
-        ws.send(stderr.toString('utf-8'))
-        ws.close()
-      }
-      if (stdout) {
-        ws.send(stdout.toString('utf-8'))
-        ws.close()
-      }
-      if(error){ 
-        ws.send(error.toString('utf-8'))
-        ws.close()
-      }
-    })
+    console.log('Conectado ao servidor ScanHealth.');
+    if (!execute) {
+      execute = true
+      sudo.exec('dism /online /cleanup-image /scanhealth', { name: 'scanhealth' }, (error, stdout, stderr) => {
+        if (stderr) {
+          ws.send(stderr.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (stdout) {
+          ws.send(stdout.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (error) {
+          ws.send(error.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+
+      })
+    }
   })
-  ws.on('close', ()=> { 
-    ws.close()
-  })
+
 }
 
 const checkHealth = () => {
-
   ws.on('connection', (ws) => {
-    console.log('Conectado ao servidor WebSocket do administrador.');
-    sudo.exec('dism /online /cleanup-image /checkhealth', { name: 'checkhealth' }, (error, stdout, stderr) => {
-      if (stderr) {
-        ws.send(stderr.toString('utf-8'))
-        ws.close()
-      }
-      if (stdout) {
-        ws.send(stdout.toString('utf-8'))
-        ws.close()
-      }
-      if(error){ 
-        ws.send(error.toString('utf-8'))
-        ws.close()
-      }
-    })
-  })
-  ws.on('close', ()=> { 
-    ws.close()
+    console.log('Conectado ao servidor CheckHealth.');
+    if (!execute) {
+      execute = true
+      sudo.exec('dism /online /cleanup-image /checkhealth', { name: 'checkhealth' }, (error, stdout, stderr) => {
+        if (stderr) {
+          ws.send(stderr.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (stdout) {
+          console.log(stdout)
+          ws.send(stdout)
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (error) {
+          ws.send(error.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+
+      })
+    }
   })
 
 }
@@ -114,33 +146,42 @@ const checkHealth = () => {
 const RestoreHealth = () => {
 
   ws.on('connection', (ws) => {
-    console.log('Conectado ao servidor WebSocket do administrador.');
-    sudo.exec('dism /online /cleanup-image /restorehealth', { name: 'restorehealth' }, (error, stdout, stderr) => {
-      if (stderr) {
-        ws.send(stderr.toString('utf-8'))
-        ws.close()
-      }
-      if (stdout) {
-        ws.send(stdout.toString('utf-8'))
-        ws.close()
-      }
-      if(error){ 
-        ws.send(error.toString('utf-8'))
-        ws.close()
-      }
-    })
+    console.log('Conectado ao servidor RestoreHealth.');
+    if (!execute) {
+      execute = true
+      sudo.exec('dism /online /cleanup-image /restorehealth', { name: 'restorehealth' }, (error, stdout, stderr) => {
+        if (stderr) {
+          ws.send(stderr.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (stdout) {
+          console.log(stdout)
+          ws.send(stdout.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+        if (error) {
+          ws.send(error.toString('utf-8'))
+          ws.terminate()
+          closeAllConnections()
+          execute = false
+        }
+
+      })
+    }
   })
-  ws.on('close', ()=> { 
-    ws.close()
-  })
+
 
 }
 
-module.exports = { 
-  ChkDsk, 
-  SystemFileCheck, 
-  ScanHealth, 
-  checkHealth, 
-  RestoreHealth, 
-  ScanHealth 
+module.exports = {
+  ChkDsk,
+  SystemFileCheck,
+  ScanHealth,
+  checkHealth,
+  RestoreHealth,
+  ScanHealth
 }
