ed987fa7db46a507cace2ccf3c6620573b69c17d
rodrigofelipe3 <rodrigo.soeu@hotmail.com>
2025-01-06 17:29:53 -0300

    feat sendinforealtime.js

--- src/controllers/sendRealTimeInfo.js
+++ src/controllers/sendRealTimeInfo.js
@@ -1,94 +1,110 @@
-const { exec } = require('child_process')
-const si = require('systeminformation')
-const WebSocket = require('ws')
+const { exec } = require('child_process');
+const si = require('systeminformation');
+const WebSocket = require('ws');
 
-const getCpuUsage = (callback) => {
+// Função assíncrona para obter o uso da CPU
+const getCpuUsage = async () => {
+    return new Promise((resolve, reject) => {
+        exec('wmic cpu get loadpercentage', async (error, stdout, stderr) => {
+            if (error) return reject(`exec error: ${error}`);
+            if (stderr) return reject(`stderr: ${stderr}`);
 
-    exec('wmic cpu get loadpercentage', async (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        const temp = await si.cpuTemperature()
-        // Processar a saída para obter o uso do processador
-        const lines = stdout.trim().split('\n');
-        const usage = lines[1] ? parseInt(lines[1].trim(), 10) : 0;
-        callback(usage, temp);
-    });
-}
-function getMemoryUsage(callback) {
-    exec('wmic OS get FreePhysicalMemory,TotalVisibleMemorySize /Value', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
 
-        // Processar a saída para obter o uso da memória
-        const lines = stdout.trim().split('\n');
-        const freeMemory = parseInt(lines[0].split('=')[1].trim(), 10);
-        const totalMemory = parseInt(lines[1].split('=')[1].trim(), 10);
-
-        const usedMemory = totalMemory - freeMemory;
-        const usedMemoryPercentage = (usedMemory / totalMemory) * 100;
-
-        // Converter para GB
-        const freeMemoryGB = (freeMemory / 1048576).toFixed(2);
-        const totalMemoryGB = (totalMemory / 1048576).toFixed(2);
-        const usedMemoryGB = (usedMemory / 1048576).toFixed(2);
-
-        callback({
-            usedMemoryPercentage: usedMemoryPercentage.toFixed(2),
-            usedMemory: usedMemoryGB,
-            totalMemory: totalMemoryGB,
-            freeMemory: freeMemoryGB,
+            // Processar a saída para obter o uso do processador
+            const lines = stdout.trim().split('\n');
+            const usage = lines[1] ? parseInt(lines[1].trim(), 10) : 0;
+            resolve({ usage});
         });
     });
-}
+};
 
-const WebSocketConnection = () => {
-    try {
-        const wss = new WebSocket.Server({ port: 443 });
-        let connectionCount = 0;
+// Função assíncrona para obter o uso da memória
+const getMemoryUsage = async () => {
+    return new Promise((resolve, reject) => {
+        exec('wmic OS get FreePhysicalMemory,TotalVisibleMemorySize /Value', (error, stdout, stderr) => {
+            if (error) return reject(`exec error: ${error}`);
+            if (stderr) return reject(`stderr: ${stderr}`);
 
-        wss.on('connection', async (ws) => {
-            if (connectionCount >= 2) {
-                ws.close(1008, 'Server is already handling the maximum number of connections'); // Código de fechamento personalizado
-                return;
-            }
-            setInterval(() => {
-                getCpuUsage((usage, temp) => {
-                    ws.send(usage.toString(), temp.toString());
-                });
-            }, 500);
+            // Processar a saída para obter o uso da memória
+            const lines = stdout.trim().split('\n');
+            const freeMemory = parseInt(lines[0].split('=')[1].trim(), 10);
+            const totalMemory = parseInt(lines[1].split('=')[1].trim(), 10);
 
-            setInterval(() => {
-                getMemoryUsage((usedMemoryPercentage, usedMemory, totalMemory, freeMemory) => {
-                    ws.send(usedMemoryPercentage.toString(), usedMemory.toString(), totalMemory.toString(), freeMemory.toString());
-                });
-            }, 500);
+            const usedMemory = totalMemory - freeMemory;
+            const usedMemoryPercentage = (usedMemory / totalMemory) * 100;
 
-            ws.on('close', () => {
-                connectionCount--;
-                console.log(`Connection closed. Current active connections: ${connectionCount}`);
+            // Converter para GB
+            const freeMemoryGB = (freeMemory / 1048576).toFixed(2);
+            const totalMemoryGB = (totalMemory / 1048576).toFixed(2);
+            const usedMemoryGB = (usedMemory / 1048576).toFixed(2);
+
+            resolve({
+                usedMemoryPercentage: usedMemoryPercentage.toFixed(2),
+                usedMemory: usedMemoryGB,
+                totalMemory: totalMemoryGB,
+                freeMemory: freeMemoryGB,
             });
         });
+    });
+};
+
+// Inicializar o WebSocket
+let wss;
+const WebSocketConnection = () => {
+    try {
+        if (!wss) {
+            wss = new WebSocket.Server({ port: 443 });
+        }
+
+        wss.on('connection', (ws) => {
+            console.log('Conectado ao cliente');
+
+            // Função para enviar mensagens assíncronas
+            const sendMessageAsync = async () => {
+                try {
+                    const cpuData = await getCpuUsage();
+                    const memoryData = await getMemoryUsage();
+                    console.log('enviando mensagem')
+                    const message = `usage:${cpuData.usage},memper:${memoryData.usedMemoryPercentage},usedmemory:${memoryData.usedMemory},totalmemory:${memoryData.totalMemory},freemem:${memoryData.freeMemory}`;
+                    
+                    if (ws.readyState === WebSocket.OPEN) {
+                        ws.send(message, (err) => {
+                            if (err) console.error(err);
+                        });
+                    }
+                } catch (error) {
+                    console.error("Erro ao enviar mensagem:", error);
+                }
+            };
+
+            // Loop assíncrono para enviar mensagens com intervalo dinâmico
+            const intervalLoop = async () => {
+                while (ws.readyState === WebSocket.OPEN) {
+                    await sendMessageAsync();
+                    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 2.5s antes da próxima execução
+                }
+            };
+
+            // Iniciar o envio de mensagens
+            intervalLoop();
+
+            // Evento de fechamento da conexão pelo cliente
+            ws.on('close', () => {
+                console.log("Fechando conexão com cliente");
+            });
+        });
+
+        process.on('SIGINT', () => {
+            console.log('Encerrando servidor WebSocket...');
+            wss.close(() => {
+                console.log('Servidor WebSocket encerrado.');
+                process.exit(0);
+            });
+        });
+
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
+};
 
-}
-
-
-getCpuUsage((usage, temp) => {
-    console.log(usage, temp.main)
-})
-
-module.exports = { WebSocketConnection }
+module.exports = { WebSocketConnection };
